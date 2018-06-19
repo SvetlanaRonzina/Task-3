799f029f958647fc65b4823f162491466f8de096
SvetlanaRonzina <swet.ronzina@yandex.ru>
2018-06-19 21:30:06 +0500

    Задача 3 выполнена
    Полностью

---
+++ Задача 3/Задача 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double X, Y;
+            Console.WriteLine("Введите координату Х точки U"); 
+            while (!double.TryParse(Console.ReadLine(), out X))
+                Console.WriteLine("Введите рациональное число");
+            Console.WriteLine("Введите координату Y точки U");
+            while (!double.TryParse(Console.ReadLine(), out Y))
+                Console.WriteLine("Введите рациональное число");
+            if ((X * X + Y * Y <= 1) && (Y >= Math.Abs(X)))
+                Console.WriteLine("U=" + Math.Sqrt ( Math.Abs(X*X-1)) + " Точка входит в область");
+            else
+                Console.WriteLine("U=" + (X +Y) +" Точка не входит в заданую область");
+
+            Console.ReadLine();
+
+        }
+    }
+}
